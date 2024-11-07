b0bbea4a38e275712c829c15be04cd753366d647
Gabrieli Mendes Nicolodi <gabiprogramadoraweb@gmail.com>
2024-11-07 11:03:19 -0300

    classificador de nivel utilizando estrutura de controle

---
+++ classificador.js
@@ -0,0 +1,23 @@
+let heroName = "Princess of Themyscira"
+let level = ""
+let xp = "6530"
+
+if(xp <= 1000){
+    level = "Ferro"
+}else if(xp >= 1001 && xp <= 2000){
+    level = "Bronze"
+}else if(xp >= 2001 && xp <= 5000){
+    level = "Prata"
+}else if(xp >= 5001 && xp <= 7000){
+    level = "Bronze"
+}else if(xp >= 7001 && xp <= 8000){
+    level = "Platina"
+}else if(xp >= 8001 && xp <= 9000){
+    level = "Ascendente"
+}else if(xp >= 9001 && xp <= 10000){
+    level = "Imortal"
+}else if(xp >= 10001){
+    level = "Radiante"
+}
+
+console.log("O Herói "+heroName+" está no nível "+level)
